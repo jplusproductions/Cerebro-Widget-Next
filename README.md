183457bff0be61d9ad3e4414e5434ef374e71edc
James R. Maxfield <Vercjames@gmail.com>
2026-02-23 16:39:12 -0700

    par setting

--- README.md
+++ README.md
@@ -1,9 +1,50 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
+# **CEREBRO WIDGET**
+*The Next Application for EYBL Statistics API*
 
-## Getting Started
+---
 
-First, run the development server:
+## 🔗 **Quick Links**
+Direct access to the live environments and developer tools.
+
+- **Dev**
+    - [Live App](https://cerebro-widget-next.vercel.app/)
+- **Production**
+    - [Live App](https://cerebro-widget-next.vercel.app/)
+---
+
+## ⚙️ **Project Requirements**
+Ensure your environment includes the following tools:
+
+- [Node](https://nodejs.org/en)
+- [Yarn](https://classic.yarnpkg.com/lang/en/docs/install/)
+
+
+
+---
+## 🧠 **Project Tech Stack**
+It’s recommended to be familiar with the core technologies used:
+
+- [Next](https://nextjs.org/)
+- [React](https://reactjs.org/)
+- [PostgreSQL](https://www.postgresql.org/)
+- [Prisma](https://www.prisma.io/)
+- [tRPC](https://trpc.io/)
+
+---
+
+
+## 🧪 **Project Commands**
+
+Run the app locally using the appropriate environment target:
+
+**Using local Docker database**
+```bash
+yarn dev 
+```
+
+Using the remote database <sub>(preferred)</sub>
 
 ```bash
-yarn dev
+yarn dev:remote-dev
+yarn dev:remote-prod
 ```
---
+++ apps-configs/env-builder.ts
@@ -0,0 +1,73 @@
+import fs from "fs"
+import path from "path"
+import dotenv from "dotenv"
+import { fileURLToPath } from "url"
+import { scripts } from "chalk-scripts"
+
+// Reconstruct __dirname for ESM
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+// Application Architecture || Define Variables
+// =======================================================================================
+// =======================================================================================
+dotenv.config({ path: path.resolve(process.cwd(), ".env") })
+const BUILD_ENV = process.env.NEXT_PUBLIC_BUILD_ENV
+const script = scripts({ name: `env:${BUILD_ENV || "unknown"}` })
+
+// Application Architecture || Define Functions
+// =======================================================================================
+// =======================================================================================
+const loadConfig = () => {
+  // NOTE: Load and parse the environment configuration from .env.local
+  const configFile = path.resolve(__dirname, "env-builder.json")
+
+  if (!fs.existsSync(configFile)) {
+    throw new Error(`Configuration file not found: ${configFile}`)
+  }
+  const configData = fs.readFileSync(configFile, "utf8")
+  return JSON.parse(configData)
+}
+
+const generateEnvContent = async () => {
+  await script.log("Generator Initialize")
+  if (!BUILD_ENV || typeof BUILD_ENV !== "string") {
+    throw new Error("`NEXT_PUBLIC_BUILD_ENV` was not included in your start script")
+  }
+
+  await script.log(`Running Build: ${BUILD_ENV}`)
+
+  const config = loadConfig()
+  const commonSettings = config.common
+  let content = `NEXT_PUBLIC_BUILD_ENV="${process.env.NEXT_PUBLIC_BUILD_ENV}"\n`
+
+  for (const key in commonSettings) {
+    content += `${key}="${commonSettings[key]}"\n`
+  }
+
+  // load the env-specific properties
+  const envSettings = config[BUILD_ENV]
+  if (envSettings) {
+    for (const key in envSettings) {
+      content += `${key}="${envSettings[key]}"\n`
+    }
+  }
+
+  return content
+}
+
+// Application Architecture || Define Executions
+// =======================================================================================
+// =======================================================================================
+void (async () => {
+  try {
+    const envContent = await generateEnvContent()
+    fs.writeFileSync("./.env", `${envContent?.trim()}\n`, "utf8")
+    await script.success("File `.env` has been created successfully.")
+  } catch (error: unknown) {
+    const message = typeof error === "object" && error !== null && "message" in error
+      ? String((error as { message: unknown }).message)
+      : "❌ Failed to write .env file"
+    await script.error(message)
+  }
+})()
